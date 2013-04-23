36e91db5605e4dfd9a130a0fec514a2244f0f8f8
mpl <mathieu.lonjaret@gmail.com>
2013-04-23 12:29:18 +0200

    wip diff

--- histo.go
+++ histo.go
@@ -53,13 +53,60 @@ func printHisto(filename string) error {
 	return nil
 }
 
+func diff(im1, im2 image.Image) float64 {
+	histo1 := Histo(im1)
+	histo2 := Histo(im2)
+	cumdiff := 0
+	cumcount := 0 // == numpixels, no?
+	for lum, count := range histo1 {
+		count2 := histo2[lum]
+		diff := count - count2
+		if diff < 0 {
+			diff = 0 - diff
+		}
+		cumdiff += diff
+		cumcount += count2
+	}
+	return float64(cumdiff) / float64(cumcount)
+}
+
+func diffFiles(file1, file2 string) (float64, error) {
+	f, err := os.Open(file1)
+	if err != nil {
+		return 0., err
+	}
+	defer f.Close()
+	im1, _, err := image.Decode(f)
+	if err != nil {
+		return 0., err
+	}
+	g, err := os.Open(file2)
+	if err != nil {
+		return 0., err
+	}
+	defer g.Close()
+	im2, _, err := image.Decode(g)
+	if err != nil {
+		return 0., err
+	}
+	return diff(im1, im2), nil
+}
+
 func main() {
-	err := printHisto("/home/mpl/Desktop/IMG_2336.JPG")
+	/*
+		err := printHisto("/home/mpl/Desktop/IMG_2336.JPG")
+		if err != nil {
+			panic(err)
+		}
+		err = printHisto("/home/mpl/Desktop/IMG_2337.JPG")
+		if err != nil {
+			panic(err)
+		}
+	*/
+	//	d, err := diffFiles("/home/mpl/Desktop/IMG_2336.JPG", "/home/mpl/Desktop/IMG_2337.JPG")
+	d, err := diffFiles("/home/mpl/Desktop/IMG_2336.JPG", "/home/mpl/Desktop/20110430_002.jpg")
 	if err != nil {
 		panic(err)
 	}
-	err = printHisto("/home/mpl/Desktop/IMG_2337.JPG")
-	if err != nil {
-		panic(err)
-	}
+	println(d)
 }
