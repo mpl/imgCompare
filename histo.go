408e3b6cca06c8ea69d7c7901d41e5ae20e1cef5
mpl <mathieu.lonjaret@gmail.com>
2013-04-25 12:46:57 +0200

    wip

--- histo.go
+++ histo.go
@@ -53,12 +53,12 @@ func printHisto(filename string) error {
 	return nil
 }
 
-func diff(im1, im2 image.Image) float64 {
+func diff1(im1, im2 image.Image) float64 {
 	histo1 := Histo(im1)
 	histo2 := Histo(im2)
 	cumdiff := 0
 	cumcount := 0 // == numpixels, no?
-	for lum, count := range histo1 {
+	for lum,count := range histo1 {
 		count2 := histo2[lum]
 		diff := count - count2
 		if diff < 0 {
@@ -68,6 +68,45 @@ func diff(im1, im2 image.Image) float64 {
 		cumcount += count2
 	}
 	return float64(cumdiff) / float64(cumcount)
+}
+
+// TODO(mpl): compensate if len(histo1) != len(histo2)
+// -> instead of looping over histo1; loop [0-256) ?
+func diff2(im1, im2 image.Image) float64 {
+	histo1 := Histo(im1)
+	histo2 := Histo(im2)
+	var cumratio float32
+	for lum,count := range histo1 {
+		count2 := histo2[lum]
+		if count == 0 || count2 == 0 {
+			continue
+		}
+		ratio := float32(count2) / float32(count)
+		if ratio > 1 {
+			ratio = 1. / ratio
+		}
+		cumratio += ratio
+	}
+	return float64(cumratio) / float64(len(histo1))
+}
+
+func diff3(im1, im2 image.Image) float64 {
+	histo1 := Histo(im1)
+	histo2 := Histo(im2)
+	var cumratio float32
+	for i := 0; i<256; i++ {
+		count1 := histo1[uint8(i)]
+		count2 := histo2[uint8(i)]
+		if count1 == 0 || count2 == 0 {
+			continue
+		}
+		ratio := float32(count2) / float32(count1)
+		if ratio > 1 {
+			ratio = 1. / ratio
+		}
+		cumratio += ratio
+	}
+	return float64(cumratio) / float64(len(histo1))
 }
 
 func diffFiles(file1, file2 string) (float64, error) {
@@ -89,24 +128,38 @@ func diffFiles(file1, file2 string) (float64, error) {
 	if err != nil {
 		return 0., err
 	}
-	return diff(im1, im2), nil
+	return diff3(im1, im2), nil
 }
 
 func main() {
-	/*
-		err := printHisto("/home/mpl/Desktop/IMG_2336.JPG")
-		if err != nil {
-			panic(err)
-		}
-		err = printHisto("/home/mpl/Desktop/IMG_2337.JPG")
-		if err != nil {
-			panic(err)
-		}
-	*/
-	//	d, err := diffFiles("/home/mpl/Desktop/IMG_2336.JPG", "/home/mpl/Desktop/IMG_2337.JPG")
-	d, err := diffFiles("/home/mpl/Desktop/IMG_2336.JPG", "/home/mpl/Desktop/20110430_002.jpg")
+/*
+	err := printHisto("/home/mpl/Desktop/IMG_2336.JPG")
+	if err != nil {
+		panic(err)
+	}
+	err = printHisto("/home/mpl/Desktop/IMG_2337.JPG")
+	if err != nil {
+		panic(err)
+	}
+*/
+	d, err := diffFiles("/home/mpl/Desktop/IMG_2336.JPG", "/home/mpl/Desktop/IMG_2337.JPG")
+	if err != nil {
+		panic(err)
+	}
+	println(d)
+	d, err = diffFiles("/home/mpl/Desktop/IMG_2336.JPG", "/home/mpl/Desktop/20110430_002.jpg")
 	if err != nil {
 		panic(err)
 	}
 	println(d)
 }
+
+/*
+diff1: +9.679229e-002 ; +2.593293e+000
+diff2: +8.978444e-001 ; +2.800825e-001
+diff3: +8.978442e-001 ; +2.800825e-001
+
+5000 5000 6
+4998 5008 0
+
+*/
