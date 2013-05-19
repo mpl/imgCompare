925879883699faf84ee719aa41a437d6b8c9e709
mpl <mathieu.lonjaret@gmail.com>
2013-05-19 17:10:34 +0200

    cross correlation -> pretty convincing

--- histo.go
+++ histo.go
@@ -6,6 +6,7 @@ import (
 	"image/color"
 	_ "image/jpeg"
 	"log"
+	"math"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -131,7 +132,7 @@ func diffFiles(file1, file2 string) (float64, error) {
 	if err != nil {
 		return 0., err
 	}
-	return diff3(im1, im2), nil
+	return diff4(im1, im2), nil
 }
 
 type compRes struct {
@@ -153,14 +154,14 @@ func diffDir(dirpath string) (matches, error) {
 	if err != nil {
 		return nil, err
 	}
-	for _, v1 := range names {
+	for k1, v1 := range names {
 		if !isJpeg.MatchString(strings.ToLower(v1)) {
 			continue
 		}
 		var res []*compRes
 		fv1 := filepath.Join(dirpath, v1)
-		for _, v2 := range names {
-			if v1 == v2 {
+		for k2, v2 := range names {
+			if k2 <= k1 {
 				continue
 			}
 			if !isJpeg.MatchString(strings.ToLower(v2)) {
@@ -178,6 +179,48 @@ func diffDir(dirpath string) (matches, error) {
 		results[fv1] = res
 	}
 	return results, nil
+}
+
+func mean(x []float64) float64 {
+	sx := 0.0
+	for _, v := range x {
+		sx += v
+	}
+	return sx / float64(len(x))
+}
+
+func denominator(x, y []float64, mx, my float64) float64 {
+	sx, sy := 0., 0.
+	for i:=0; i<len(x); i++ {
+		sx += (x[i] - mx) * (x[i] - mx)
+		sy += (y[i] - my) * (y[i] - my)
+	}
+	return math.Sqrt(sx * sy)
+}
+
+func xCorrelation(x, y []float64) float64 {
+	mx := mean(x)
+	my := mean(y)
+
+	denom := denominator(x, y, mx, my)
+
+	sxy := 0.0
+	for i:=0; i<len(x); i++ {
+		sxy += (x[i] - mx) * (y[i] - my);
+	}
+	return sxy / denom
+}
+
+func diff4(im1, im2 image.Image) float64 {
+	histo1 := Histo(im1)
+	histo2 := Histo(im2)
+	var x []float64
+	var y []float64
+	for i:=0; i<256; i++ {
+		x = append(x, float64(histo1[uint8(i)]))
+		y = append(y, float64(histo2[uint8(i)]))
+	}
+	return xCorrelation(x, y)
 }
 
 func main() {
@@ -201,7 +244,7 @@ func main() {
 	}
 	println(d)
 */
-	_, err := diffDir("/home/mpl/Desktop/pics/passur/pleubian/")
+	_, err := diffDir("/home/mpl/Pictures/pleubian/")
 	if err != nil {
 		panic(err)
 	}
@@ -237,4 +280,31 @@ diff3: +8.978442e-001 ; +2.800825e-001
 (20110430_002.jpg, 20110430_003.jpg) : 0.398103
 (20110430_002.jpg, 20110424_001.jpg) : 0.399792
 -> diff3 not convincing
+
+diff4:
+(maison.jpg, plage.jpg) : 0.791355
+(maison.jpg, voiture.jpg) : 0.575118
+(maison.jpg, tour.jpg) : -0.243935
+(maison.jpg, vipere.jpg) : 0.054535
+(plage.jpg, voiture.jpg) : 0.427355
+(plage.jpg, tour.jpg) : -0.184758
+(plage.jpg, vipere.jpg) : 0.082849
+(voiture.jpg, tour.jpg) : 0.020259
+(voiture.jpg, vipere.jpg) : -0.165653
+(tour.jpg, vipere.jpg) : 0.412750
+
+(maison.jpg, plage.jpg) : 0.791355
+(maison.jpg, voiture.jpg) : 0.575118
+(plage.jpg, voiture.jpg) : 0.427355
+(tour.jpg, vipere.jpg) : 0.412750
+(plage.jpg, vipere.jpg) : 0.082849
+(maison.jpg, vipere.jpg) : 0.054535
+(voiture.jpg, tour.jpg) : 0.020259
+(voiture.jpg, vipere.jpg) : -0.165653
+(plage.jpg, tour.jpg) : -0.184758
+(maison.jpg, tour.jpg) : -0.243935
+
+-> fuck yeah!
+
+
 */
